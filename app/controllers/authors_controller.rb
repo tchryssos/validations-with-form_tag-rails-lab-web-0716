56bc8fac17aff8751a048f0dd98796d051bc5a7b
Troy Chryssos <github email address>
2016-08-20 18:51:46 -0400

    Developed blog validation system which not only checked for validations, but also displayed errors to user and highlighted error fields.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,21 +1,43 @@
 class AuthorsController < ApplicationController
+
+  before_action :set_author, only: [:edit, :update, :show]
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
     @author = Author.new
   end
 
-  def create
-    @author = Author.create(author_params)
+  def edit
+    binding.pry
+  end
 
-    redirect_to author_path(@author)
+  def create
+    @author = Author.new(author_params)
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @author.update(author_params)
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
 
   def author_params
     params.permit(:name, :email, :phone_number)
+  end
+
+  def set_author
+    @author=Author.find(params[:id])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,46 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:edit, :update, :show]
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post=Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
-    @post = Post.find(params[:id])
+  end
+
+  def index
+  end
+
+  def new
+    @post=Post.new(post_params)
   end
 
   def update
-    @post = Post.find(params[:id])
 
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+
+  def set_post
+    @post=Post.find(params[:id])
   end
 end
